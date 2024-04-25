48f64ff7f6def712c3293ec7716d6b18ad7115bf
pyro <gunkotim3@gmail.com>
2024-04-25 17:02:22 +0700

    add check a config

--- lab4/src/main/java/ru/nsu/gunko/Main.java
+++ lab4/src/main/java/ru/nsu/gunko/Main.java
@@ -8,6 +8,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Need conf...");
+            System.exit(1);
+        }
+
         Window window = new Window();
         Preparer preparer = new Preparer();
 
